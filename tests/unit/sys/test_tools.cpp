688a954cd41c31e9010bcdfe69ed3e4aba3a8dcb
S. V. Paulauskas <stan@xia.com>
2020-12-02 16:23:50 -0500

    PIXIE-33: Adding tests (kinda) for the timing functions.
    There's no really good way to test these functions. They depend
    on values that we can't independently verify.

--- tests/unit/sys/test_tools.cpp
+++ tests/unit/sys/test_tools.cpp
@@ -5,8 +5,17 @@
 #include "doctest.h"
 #include "pixie16sys_common.h"
 
+#include <chrono>
+
 TEST_SUITE("sys/tools.c") {
-    TEST_CASE("get_ns_per_cycle") { CHECK(false); }
+    TEST_CASE("get_ns_per_cycle") {
+        //NOTE: This function isn't really testable, since there's no independent way to verify the results. We'll work
+        //        under the assumption that we get a result that's greater than 0 and be happy with it.
+        auto* result = new double;
+        CHECK(0 == get_ns_per_cycle(result));
+        CHECK(*result > 0);
+        delete result;
+    }
     TEST_CASE("Pixie_Print_MSG") {}
     TEST_CASE("SYS16_ClrBit") {
         CHECK(10 == SYS16_ClrBit(10, 10));
@@ -47,5 +56,15 @@ TEST_SUITE("sys/tools.c") {
         CHECK_FALSE(SYS32_TstBit(10, 524288));
         CHECK_FALSE(SYS32_TstBit(33, 4294967295));
     }
-    TEST_CASE("wait_for_a_short_time") { CHECK(false); }
+    TEST_CASE("wait_for_a_short_time") {
+        /// NOTE: This function doesn't have a good way to test. Just as the other time related function above.
+        ///          We'll just make sure that what we are both less than the value we passed in in the event we used
+        ///          the usleep option, and greater than 0.
+        auto start = std::chrono::steady_clock::now();
+        wait_for_a_short_time(10);
+        double result =
+                std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - start).count();
+        CHECK(result <= 10.5);
+        CHECK(result > 0);
+    }
 }
