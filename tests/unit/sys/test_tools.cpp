be44cdb5453c78862e4f0342629824ffdd3c35a1
S. V. Paulauskas <stan@xia.com>
2020-12-03 11:05:26 -0500

    PIXIE-33: Adding test for Pixie_Print_MSG
    This test is a little involved since we generate a random value,
    write the value to the file, then read back the last line of
    the file. It at least verifies functionality.

--- tests/unit/sys/test_tools.cpp
+++ tests/unit/sys/test_tools.cpp
@@ -4,8 +4,11 @@
 /// @date November 23, 2020
 #include "doctest.h"
 #include "pixie16sys_common.h"
+#include "pixie16sys_export.h"
 
 #include <chrono>
+#include <fstream>
+#include <random>
 
 TEST_SUITE("sys/tools.c") {
     TEST_CASE("get_ns_per_cycle") {
@@ -16,7 +19,20 @@ TEST_SUITE("sys/tools.c") {
         CHECK(*result > 0);
         delete result;
     }
-    TEST_CASE("Pixie_Print_MSG") {}
+    TEST_CASE("Pixie_Print_MSG") {
+        std::random_device rd;
+        auto random_val = std::fabs(rd());
+        char msg[1024];
+        sprintf(msg, "Unit test: %f", random_val);
+        CHECK(Pixie_Print_MSG(msg) == 0);
+        std::ifstream file("Pixie16msg.txt", std::ios::in);
+        REQUIRE(file.is_open());
+        //From: http://cplusplus.com/forum/general/108679/#msg591060
+        std::string line;
+        while (file >> std::ws && std::getline(file, line));
+        CHECK(line.find(std::to_string(random_val)) != std::string::npos);
+        file.close();
+    }
     TEST_CASE("SYS16_ClrBit") {
         CHECK(10 == SYS16_ClrBit(10, 10));
         CHECK(8 == SYS16_ClrBit(1, 10));
@@ -63,7 +79,8 @@ TEST_SUITE("sys/tools.c") {
         auto start = std::chrono::steady_clock::now();
         wait_for_a_short_time(10);
         double result =
-                std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - start).count();
+                std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - start)
+                        .count();
         CHECK(result <= 10.5);
         CHECK(result > 0);
     }
