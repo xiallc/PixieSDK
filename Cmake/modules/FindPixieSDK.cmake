10ecab83e1840ebccf4c9f2174c204c03a5eae7d
S. V. Paulauskas <stan@xia.com>
2021-02-08 14:44:32 -0500

    P16-24: Adding FindPixieSDK.cmake so that users have a way to find our library w/ Cmake

---
+++ Cmake/modules/FindPixieSDK.cmake
@@ -0,0 +1,26 @@
+# Find the Pixie SDK
+#
+# XIA_LIBRARY_DIR
+# XIA_INCLUDE_DIR
+# XIA_LIBRARIES
+
+find_path(XIA_LIBRARY_DIR
+        NAMES libPixie16App.so libPixie16Sys.so
+        HINTS $ENV{XIA_PIXIE_SDK}
+        PATHS /usr/local/xia/pixie_sdk
+        PATH_SUFFIXES lib
+        DOC "Path to pixie library.")
+
+find_path(XIA_INCLUDE_DIR
+        NAMES pixie16app_defs.h pixie16sys_defs.h
+        HINTS $ENV{XIA_PIXIE_SDK}
+        PATHS /usr/local/xia/pixie_sdk
+        PATH_SUFFIXES include
+        DOC "Path to XIA includes.")
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PIXIE_SDK DEFAULT_MSG XIA_LIBRARY_DIR)
+
+if (PIXIE_SDK_FOUND)
+    set(XIA_LIBRARIES -lPixie16App -lPixie16Sys)
+endif ()
