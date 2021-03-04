7003e2bf03a5be0bea42f5a10c5519176dbefc7c
S. V. Paulauskas <stan@xia.com>
2021-03-04 09:44:14 -0500

    P16-95: Updating header file header consistency and dates

--- sdk/include/pixie16sys_globals.h
+++ sdk/include/pixie16sys_globals.h
@@ -1,57 +1,44 @@
-#ifndef PIXIE16SYS_GLOBALS_H
-#define PIXIE16SYS_GLOBALS_H
-
-/*----------------------------------------------------------------------
-* Copyright (c) 2005 - 2020, XIA LLC
+/**----------------------------------------------------------------------
+* Copyright (c) 2005 - 2021, XIA LLC
 * All rights reserved.
 *
-* Redistribution and use in source and binary forms, 
-* with or without modification, are permitted provided 
+* Redistribution and use in source and binary forms,
+* with or without modification, are permitted provided
 * that the following conditions are met:
 *
-*   * Redistributions of source code must retain the above 
-*     copyright notice, this list of conditions and the 
+*   * Redistributions of source code must retain the above
+*     copyright notice, this list of conditions and the
 *     following disclaimer.
-*   * Redistributions in binary form must reproduce the 
-*     above copyright notice, this list of conditions and the 
-*     following disclaimer in the documentation and/or other 
+*   * Redistributions in binary form must reproduce the
+*     above copyright notice, this list of conditions and the
+*     following disclaimer in the documentation and/or other
 *     materials provided with the distribution.
 *   * Neither the name of XIA LLC nor the names of its
 *     contributors may be used to endorse or promote
-*     products derived from this software without 
+*     products derived from this software without
 *     specific prior written permission.
 *
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-* IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE 
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
-* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
-* THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+* THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 * SUCH DAMAGE.
-*----------------------------------------------------------------------*/
+*----------------------------------------------------------------------**/
+/// @file pixie16sys_globals.h
+/// @brief Declaration of pixie16 library global variables.
+/// @author H. Tan
+/// @date August 21, 2006
 
-/******************************************************************************
-*
-* File Name:
-*
-*		pixie16sys_globals.h
-*
-* Description:
-*
-*		Declaration of pixie16 library global variables.
-*
-* $Rev$
-* $Id$
-******************************************************************************/
-
-// If this is compiled by a C++ compiler, make it
-// clear that these are C routines.
+#ifndef XIA_PIXIE16SYS_GLOBALS_H
+#define XIA_PIXIE16SYS_GLOBALS_H
 #ifdef __cplusplus
 extern "C" {
 #endif
