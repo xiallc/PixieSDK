a2404da659d2c5a4146c63d4bf7ae8a904f1c0d6
S. V. Paulauskas <stan@xia.com>
2021-04-08 09:22:36 -0400

    P16-162: Updating to show these are legacy tests. Remove useless tests.
    Combining tests to a single folder just as with the SDK.

--- tests/unit/legacy/test_pixie16app.cpp
+++ tests/unit/legacy/test_pixie16app.cpp
@@ -44,28 +44,6 @@ TEST_SUITE("app/pixie16app.c") {
     TEST_CASE("APP32_TstBit") {
         CHECK(APP32_TstBit(19, 1 << 19));
     }
-    TEST_CASE("Pixie16AcquireADCTrace") {
-        /// Cannot test this function in its current form.
-    }
-    TEST_CASE("Pixie16AcquireBaselines") {
-        CHECK(Pixie16AcquireBaselines(1) == -1);
-    }
-    TEST_CASE("Pixie16AdjustOffsets") {
-        CHECK(Pixie16AdjustOffsets(1) == -1);
-    }
-    TEST_CASE("Pixie16BLcutFinder") {
-        unsigned int retval;
-        CHECK(Pixie16BLcutFinder(0, 1, &retval) == -1);
-    }
-    TEST_CASE("Pixie16BootModule") {
-        CHECK(Pixie16BootModule(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, 1, 0x7F) == -1);
-    }
-    TEST_CASE("Pixie16CheckExternalFIFOStatus") {
-        CHECK(Pixie16CheckExternalFIFOStatus(nullptr, 1) == -1);
-    }
-    TEST_CASE("Pixie16CheckRunStatus") {
-        CHECK(Pixie16CheckRunStatus(1) == -1);
-    }
     TEST_CASE("Pixie16complexFFT") {
         static const size_t length = 256;
         double data[length] = {
@@ -423,30 +401,6 @@ TEST_SUITE("app/pixie16app.c") {
                 CHECK(expected[i] == doctest::Approx(result[i]).epsilon(0.01));
         }
     }
-    TEST_CASE("Pixie16ControlTaskRun") {
-        CHECK(Pixie16ControlTaskRun(300, 1, 1000) == -1);
-    }
-    TEST_CASE("Pixie16CopyDSPParameters") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16EMbufferIO") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16EndRun") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ExitSystem") {
-        CHECK(Pixie16ExitSystem(40) == -1);
-    }
-    TEST_CASE("Pixie16GetEventsInfo") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16GetModuleEvents") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16IMbufferIO") {
-        CHECK(false);
-    }
     TEST_CASE("Pixie16InitSystem") {
         SUBCASE("Tests around module number checking") {
             CHECK(Pixie16InitSystem(0, nullptr, 1) == -1);
@@ -462,35 +416,6 @@ TEST_SUITE("app/pixie16app.c") {
             CHECK(Pixie16InitSystem(1, slot_map, 1) == 0);
         }
     }
-    TEST_CASE("Pixie16LoadDSPParametersFromFile") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ProgramFippi") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16RampOffsetDACs") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadCSR") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadDataFromExternalFIFO") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadHistogramFromFile") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadHistogramFromModule") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadListModeTrace") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadModuleInfo") {
-        unsigned short dummy;
-        unsigned int dummy1;
-        CHECK(Pixie16ReadModuleInfo(40, &dummy, &dummy1, &dummy, &dummy) == -1);
-    }
     TEST_CASE("Pixie16ReadSglChanADCTrace") {
         unsigned short dummy;
         unsigned short slot_map[1] = {5};
@@ -499,50 +424,5 @@ TEST_SUITE("app/pixie16app.c") {
         Pixie16InitSystem(1, slot_map, 1);
         CHECK(Pixie16ReadSglChanADCTrace(&dummy, 0, 0, 50) == -2);
         CHECK(Pixie16ReadSglChanADCTrace(&dummy, MAX_ADC_TRACE_LEN + 1, 0, 15) == -3);
-    }
-    TEST_CASE("Pixie16ReadSglChanBaselines") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadSglChanPar") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadSglModPar") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16ReadStatisticsFromModule") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16RegisterIO") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16SaveDSPParametersToFile") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16SaveHistogramToFile") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16SetDACs") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16SetOfflineVariant") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16StartHistogramRun") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16StartListModeRun") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16TauFinder") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16WriteCSR") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16WriteSglChanPar") {
-        CHECK(false);
-    }
-    TEST_CASE("Pixie16WriteSglModPar") {
-        CHECK(false);
     }
 }
