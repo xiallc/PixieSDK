d016f4bd3260054bcf306424dcde3b89dbf5c5ff
S. V. Paulauskas <stan@xia.com>
2021-04-19 16:17:31 -0400

    P16-145: Adding unit tests for error class and functions

---
+++ tests/unit/src/test_pixie_error.cpp
@@ -0,0 +1,56 @@
+/**----------------------------------------------------------------------
+* Copyright (c) 2005 - 2021, XIA LLC
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms,
+* with or without modification, are permitted provided
+* that the following conditions are met:
+*
+*   * Redistributions of source code must retain the above
+*     copyright notice, this list of conditions and the
+*     following disclaimer.
+*   * Redistributions in binary form must reproduce the
+*     above copyright notice, this list of conditions and the
+*     following disclaimer in the documentation and/or other
+*     materials provided with the distribution.
+*   * Neither the name of XIA LLC nor the names of its
+*     contributors may be used to endorse or promote
+*     products derived from this software without
+*     specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+* THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*----------------------------------------------------------------------**/
+/// @file test_pixie_error.cpp
+/// @brief
+/// @author S. V. Paulauskas
+/// @date April 19, 2021
+
+#include "doctest.h"
+#include "pixie_error.hpp"
+
+TEST_SUITE("xia::pixie::error") {
+    TEST_CASE("Result Generation") {
+        SUBCASE("Valid Error Code") {
+            CHECK(xia::pixie::error::api_result_text(xia::pixie::error::code::unknown_error) ==
+                  "unknown error");
+            CHECK(xia::pixie::error::api_result(xia::pixie::error::code::unknown_error) == 900);
+        }
+        SUBCASE("Invalid Error Code") {
+            CHECK(xia::pixie::error::api_result_text(xia::pixie::error::code::last) ==
+                  "bad error code");
+            CHECK(xia::pixie::error::api_result(xia::pixie::error::code::last) == 990);
+        }
+    }
+}
