8f3b3371517b4d4e442c95cb763de21d2474220b
S. V. Paulauskas <stan@xia.com>
2021-05-18 16:35:54 -0400

    P16-254: Adding more test configurations and XDT test.

--- tests/functional/src/test_parameter_read_write.cpp
+++ tests/functional/src/test_parameter_read_write.cpp
@@ -44,41 +44,85 @@
 #include <pixie_log.hpp>
 #include <simulation.hpp>
 
+static xia::pixie::sim::crate crate;
+
+static const std::vector<std::string> module_defs = {
+    "device-number=0,slot=2, revision=13, eeprom-format=1, serial-num=250, num-channels=16, adc-msps=100, adc-bits=16, adc-clk-div=1",
+    "device-number=1,slot=3, revision=15, eeprom-format=1, serial-num=1000, num-channels=16, adc-msps=250, adc-bits=16, adc-clk-div=1",
+    "device-number=2,slot=4, revision=15, eeprom-format=1, serial-num=1001, num-channels=16, adc-msps=500, adc-bits=14, adc-clk-div=1"
+};
+
 static const std::vector<std::string> firmware_defs = {
+    "version=sim, revision=13, adc-msps=100, adc-bits=16, device=sys, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_sys.bin",
+    "version=sim, revision=13, adc-msps=100, adc-bits=16, device=fippi, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_fippi.bin",
+    "version=sim, revision=13, adc-msps=100, adc-bits=16, device=dsp, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.ldr",
+    "version=sim, revision=13, adc-msps=100, adc-bits=16, device=var, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.var",
     "version=sim, revision=15, adc-msps=250, adc-bits=16, device=sys, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_sys.bin",
     "version=sim, revision=15, adc-msps=250, adc-bits=16, device=fippi, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_fippi.bin",
     "version=sim, revision=15, adc-msps=250, adc-bits=16, device=dsp, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.ldr",
-    "version=sim, revision=15, adc-msps=250, adc-bits=16, device=var, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.var"};
+    "version=sim, revision=15, adc-msps=250, adc-bits=16, device=var, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.var",
+    "version=sim, revision=15, adc-msps=500, adc-bits=14, device=sys, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_sys.bin",
+    "version=sim, revision=15, adc-msps=500, adc-bits=14, device=fippi, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_fippi.bin",
+    "version=sim, revision=15, adc-msps=500, adc-bits=14, device=dsp, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.ldr",
+    "version=sim, revision=15, adc-msps=500, adc-bits=14, device=var, file=@PROJECT_SOURCE_DIR@/tests/assets/simulation/sim_dsp.var",
+};
 
-void init_logging() {
+void setup_simulation() {
     xia::logging::start("log", "stdout", xia::log::level::debug, false);
-}
-
-void setup_simulation(xia::pixie::sim::crate& crate, const size_t& revision, const size_t& num_channels, const size_t& msps,
-                      const size_t& bits) {
-    init_logging();
     std::stringstream def;
-    def << "device-number=0, slot=2, revision=" << revision
-        << ", eeprom-format=1, serial-num=1000, num-channels=" << num_channels
-        << ", adc-msps=" << msps << ", adc-bits=" << bits << ", adc-clk-div=1";
+    for (const auto& mod_def : module_defs)
+        def << mod_def << std::endl;
     xia::pixie::sim::load_module_defs(def);
     for (const auto& item : firmware_defs) {
         auto fw = xia::pixie::firmware::parse(item, ',');
         xia::pixie::firmware::add(crate.firmware, fw);
     }
-    crate.initialize(1, true);
+    crate.initialize(module_defs.size(), false);
     crate.set_firmware();
     crate.probe();
 }
 
 TEST_SUITE("xia::pixie::channel") {
     TEST_CASE("VOFFSET") {
-        xia::pixie::sim::crate crate;
-        setup_simulation(crate, 15, 16, 250, 16);
+        setup_simulation();
         const double expected_par = 0.77;
         const size_t expected_var = 49588;
-        crate[0].write("VOFFSET", 0, expected_par);
-        CHECK(crate[0].read_var("OffsetDAC", 0, 0) == expected_var);
-        CHECK(doctest::Approx(crate[0].read("VOFFSET", 0)).epsilon(0.001) == expected_par);
+        crate[1].write("VOFFSET", 0, expected_par);
+        CHECK(crate[1].read_var("OffsetDAC", 0, 0) == expected_var);
+        CHECK(doctest::Approx(crate[1].read("VOFFSET", 0)).epsilon(0.001) == expected_par);
+    }
+    TEST_CASE("XDT") {
+        SUBCASE("100 MSPS - Too Small") {
+            const double value = 0.0001;
+            const double expected_par = 0.06;
+            const double expected_var = 6;
+            crate[0].write("XDT", 0, value);
+            CHECK(crate[0].read_var("Xwait", 0, 0) == expected_var);
+            CHECK(crate[0].read("XDT", 0) == expected_par);
+        }
+        SUBCASE("250 MSPS - Too Small") {
+            const double value = 0.0001;
+            const double expected_par = 0.08;
+            const double expected_var = 8;
+            crate[1].write("XDT", 0, value);
+            CHECK(crate[1].read_var("Xwait", 0, 0) == expected_var);
+            CHECK(crate[1].read("XDT", 0) == expected_par);
+        }
+        SUBCASE("250 MSPS") {
+            const size_t value = 15;
+            const size_t expected_var = 1504;
+            const double expected_par = 15.04;
+            crate[1].write("XDT", 0, value);
+            CHECK(crate[1].read_var("Xwait", 0, 0) == expected_var);
+            CHECK(doctest::Approx(crate[1].read("XDT", 0)) == expected_par);
+        }
+        SUBCASE("500 MSPS") {
+            const size_t value = 14;
+            const size_t expected_var = 1404;
+            const double expected_par = 14.04;
+            crate[2].write("XDT", 0, value);
+            CHECK(crate[2].read_var("Xwait", 0, 0) == expected_var);
+            CHECK(doctest::Approx(crate[2].read("XDT", 0)) == expected_par);
+        }
     }
 }
