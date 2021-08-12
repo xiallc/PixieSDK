9162bd54558e70f31bcbb4e0e16675a6104f5b5a
S. V. Paulauskas <stan@xia.com>
2021-08-12 17:18:57 -0400

    P16-299: Update FindPixieSDK.cmake to find the new libraries and headers.
    The old implementation didn't find anything correctly now that we've finalized
    our library and header paths.
    
    We also resolve a bug in the install that stopped the headers from ending up
    in the right spot.

--- cmake/modules/FindPixieSDK.cmake
+++ cmake/modules/FindPixieSDK.cmake
@@ -16,28 +16,73 @@
 
 # @file FindPixieSDK.cmake
 # @brief Find the PixieSDK installed on the user's system.
+#
+# This module defines the following variables:
+# PIXIESDK_LIBRARY_DIR -> Full path to the PixieSDK lib directory.
+# PIXIESDK_INCLUDE_DIR -> Full path to the PixieSDK include directory.
+# PIXIESDK_LIBRARY -> Full path to the PixieSDK library.
+# PIXIESDK_LIBRARIES -> Library definition with dependencies for use when linking.
+# PIXIESDK_LEGACY_LIBRARY -> Full path to the legacy C API library: Pixie16App.a.
+# PIXIESDK_LEGACY_LIBRARIES -> Library definition with dependencies for use when linking.
+# PIXIESDK_LEGACY_INCLUDE_DIR -> Full path legacy C API headers.
+# PIXIESDK_PIXIE16_LIBRARY -> Full path to the C++ compatibility wrapper: Pixie16Api.so.
+# PIXIESDK_PIXIE16_LIBRARIES -> Library definition with dependencies for use when linking.
+#
 
-# XIA_LIBRARY_DIR
-# XIA_INCLUDE_DIR
-# XIA_LIBRARIES
-
-find_path(XIA_LIBRARY_DIR
-        NAMES libPixieSDK.so
+find_path(PIXIESDK_LIBRARY_DIR
+        NAMES libPixieSDK.a PixieSDK.lib libPixie16App.a Pixie16App.lib
         HINTS $ENV{XIA_PIXIE_SDK}
-        PATHS /usr/local/xia/pixie_sdk
+        PATHS /usr/local/xia/PixieSDK "C:/Program Files (x86)/PixieSDK"
         PATH_SUFFIXES lib
-        DOC "Path to pixie library.")
+        DOC "Full path to the PixieSDK lib directory."
+        )
 
-find_path(XIA_INCLUDE_DIR
-        NAMES pixie16app_defs.h pixie16sys_defs.h
+find_path(PIXIESDK_INCLUDE_DIR
+        NAMES pixie legacy
         HINTS $ENV{XIA_PIXIE_SDK}
-        PATHS /usr/local/xia/pixie_sdk
+        PATHS ${PIXIESDK_LIBRARY_DIR}/../
         PATH_SUFFIXES include
-        DOC "Path to XIA includes.")
+        DOC "Full path to the PixieSDK include directory."
+        )
+
+find_library(PIXIESDK_LIBRARY
+        NAMES PixieSDK
+        HINTS $ENV{XIA_PIXIE_SDK}/lib ${PIXIESDK_LIBRARY_DIR}
+        DOC "Full path to the PixieSDK library."
+        )
+
+find_library(PIXIESDK_LEGACY_LIBRARY
+        NAMES Pixie16App
+        HINTS $ENV{XIA_PIXIE_SDK}/lib ${PIXIESDK_LIBRARY_DIR}
+        DOC "Full path to the legacy C API library: Pixie16App.a."
+        )
+
+find_path(PIXIESDK_LEGACY_INCLUDE_DIR
+        NAMES pixie16app_defs.h
+        HINTS $ENV{XIA_PIXIE_SDK}/include
+        PATHS ${PIXIESDK_INCLUDE_DIR}
+        PATH_SUFFIXES legacy
+        DOC "Full path legacy C API headers."
+        )
+
+find_library(PIXIESDK_PIXIE16_LIBRARY
+        NAMES Pixie16Api
+        HINTS $ENV{XIA_PIXIE_SDK}/lib ${PIXIESDK_LIBRARY_DIR}
+        DOC "Full path to the C++ compatibility wrapper: Pixie16Api.so."
+        )
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PIXIE_SDK DEFAULT_MSG XIA_LIBRARY_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PixieSDK DEFAULT_MSG
+        PIXIESDK_LIBRARY_DIR PIXIESDK_INCLUDE_DIR)
 
-if (PIXIE_SDK_FOUND)
-    set(XIA_LIBRARIES -lPixieSDK)
+if (PixieSDK_FOUND)
+    if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+        set(PIXIESDK_LIBRARIES PixieSdk dl m pthread)
+        set(PIXIESDK_PIXIE16_LIBRARIES Pixie16Api PixieSdk dl m pthread)
+        set(PIXIESDK_LEGACY_LIBRARIES Pixie16App dl m)
+    elseif (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+        set(PIXIESDK_LIBRARIES PixieSdk)
+        set(PIXIESDK_PIXIE16_LIBRARIES Pixie16Api PixieSdk)
+        set(PIXIESDK_LEGACY_LIBRARIES Pixie16App winmm)
+    endif ()
 endif ()
