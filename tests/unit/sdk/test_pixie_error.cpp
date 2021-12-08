5e9518d5c5fc73388bfbe98ab57e9bca57a2e036
S. V. Paulauskas <stan@xia.com>
2021-12-08 09:19:27 -0500

    P16-446: Expanding unit test coverage for error.cpp
    We now have 100% test coverage for the file.

--- tests/unit/sdk/test_pixie_error.cpp
+++ tests/unit/sdk/test_pixie_error.cpp
@@ -24,16 +24,77 @@
 #include <pixie/error.hpp>
 
 TEST_SUITE("xia::pixie::error") {
-    TEST_CASE("Result Generation") {
-        SUBCASE("Valid Error Code") {
-            CHECK(xia::pixie::error::api_result_text(xia::pixie::error::code::unknown_error) ==
-                  "unknown error");
-            CHECK(xia::pixie::error::api_result(xia::pixie::error::code::unknown_error) == 900);
+    TEST_CASE("Error class") {
+        SUBCASE("Test constructor taking char*") {
+            xia::pixie::error::error err(xia::pixie::error::code::device_dma_busy, "test char");
+            CHECK(err.result_text() == "device dma busy");
+            CHECK(err.result() == 507);
+            CHECK(err.return_code() == -507);
+            std::ostringstream ostringstream;
+            err.output(ostringstream);
+            CHECK(ostringstream.str() == "error: code:507 : test char");
         }
-        SUBCASE("Invalid Error Code") {
-            CHECK(xia::pixie::error::api_result_text(xia::pixie::error::code::last) ==
-                  "bad error code");
-            CHECK(xia::pixie::error::api_result(xia::pixie::error::code::last) == 990);
+        SUBCASE("Test constructor taking string") {
+            xia::pixie::error::error err(xia::pixie::error::code::device_dma_busy,
+                                         std::string("test str"));
+            CHECK(err.result_text() == "device dma busy");
+            CHECK(err.result() == 507);
+            CHECK(err.return_code() == -507);
+            std::ostringstream ostringstream;
+            err.output(ostringstream);
+            CHECK(ostringstream.str() == "error: code:507 : test str");
+        }
+        SUBCASE("Test constructor taking ostringstream") {
+            std::ostringstream msg;
+            msg << "test stream";
+            xia::pixie::error::error err(xia::pixie::error::code::device_dma_busy, msg);
+            CHECK(err.result_text() == "device dma busy");
+            CHECK(err.result() == 507);
+            CHECK(err.return_code() == -507);
+            std::ostringstream ostringstream;
+            err.output(ostringstream);
+            CHECK(ostringstream.str() == "error: code:507 : test stream");
+        }
+    }
+    TEST_CASE("Output Streams") {
+        SUBCASE("ostringstreams") {
+            std::ostringstream ostringstream;
+            xia::pixie::error::error err(xia::pixie::error::code::device_dma_busy, "test what");
+            ostringstream << err;
+            CHECK(ostringstream.str() == "result=507, test what");
+        }
+        SUBCASE("ostreams") {
+            std::stringbuf stringbuf;
+            xia::pixie::error::error err(xia::pixie::error::code::device_dma_busy, "test what");
+            std::ostream os(&stringbuf);
+            os << err;
+            CHECK(stringbuf.str() == "error: code:507 : test what");
+        }
+    }
+    TEST_CASE("Error text generation") {
+        CHECK(xia::pixie::error::api_result_text(xia::pixie::error::code::unknown_error) ==
+              "unknown error");
+        CHECK(xia::pixie::error::api_result_text(xia::pixie::error::code::last) ==
+              "bad error code");
+        CHECK(xia::pixie::error::api_result_text(1234) == "bad error code");
+        CHECK(xia::pixie::error::api_result_text(-900) == "unknown error");
+    }
+    TEST_CASE("Result Generation") {
+        CHECK(xia::pixie::error::api_result(xia::pixie::error::code::unknown_error) == 900);
+        CHECK(xia::pixie::error::api_result(xia::pixie::error::code::last) == 990);
+    }
+    TEST_CASE("Check return_code wrappers") {
+        SUBCASE("Generate Return code") {
+            CHECK(xia::pixie::error::return_code(1234) == -1234);
+        }
+        SUBCASE("Bad Alloc Return Code") {
+            CHECK(xia::pixie::error::return_code_bad_alloc_error() == -902);
+        }
+        SUBCASE("Unknown Error Return Code") {
+            CHECK(xia::pixie::error::return_code_unknown_error() == -900);
+        }
+        SUBCASE("Not Supported Return Code") {
+            CHECK(xia::pixie::error::return_code_not_supported() == -803);
         }
     }
     TEST_CASE("Result_codes size matches code::last") {
