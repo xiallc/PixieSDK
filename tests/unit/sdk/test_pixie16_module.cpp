2852555577dfe3c84ecc6e0b4f7d0a6e3fe9b4d3
S. V. Paulauskas <stan@xia.com>
2022-02-04 05:47:34 -0500

    P16-462: Organizing tests into subcases for clarity

--- tests/unit/sdk/test_pixie16_module.cpp
+++ tests/unit/sdk/test_pixie16_module.cpp
@@ -32,27 +32,25 @@
 using crate_error = xia::pixie::crate::error;
 using xia_log = xia::log;
 
-static const std::vector<std::string> module_def =
-{
-    "device-number=0 slot=2 revision=15 eeprom-format=1 serial-num=1034 num-channels=16 " \
+static const std::vector<std::string> module_def = {
+    "device-number=0 slot=2 revision=15 eeprom-format=1 serial-num=1034 num-channels=16 "
     "adc-bits=14 adc-msps=500 adc-clk-div=5",
-    "device-number=1 slot=6 revision=15 eeprom-format=1 serial-num=1035 num-channels=16 " \
+    "device-number=1 slot=6 revision=15 eeprom-format=1 serial-num=1035 num-channels=16 "
     "adc-bits=14 adc-msps=500 adc-clk-div=5",
-    "device-number=2 slot=10 revision=17 eeprom-format=1 serial-num=2034 num-channels=32 " \
-    "adc-bits=14 adc-msps=500 adc-clk-div=5"
-};
+    "device-number=2 slot=10 revision=17 eeprom-format=1 serial-num=2034 num-channels=32 "
+    "adc-bits=14 adc-msps=500 adc-clk-div=5"};
 
 static const size_t test_modules = module_def.size();
 
 static void test_setup() {
-    xia::logging::start("log", "stdout", xia::log::level::debug, false);
+    xia::logging::start("log", "stdout", xia::log::level::off, false);
     for (auto& def : module_def) {
         xia::pixie::sim::add_module_def(def);
     }
 }
 
 TEST_SUITE("Crate: modules") {
-   TEST_CASE("SETUP") {
+    TEST_CASE("SETUP") {
         test_setup();
     }
     TEST_CASE("none") {
@@ -64,11 +62,11 @@ TEST_SUITE("Crate: modules") {
         CHECK(crate.offline.size() == 0);
         CHECK(crate.busy() == false);
         CHECK(crate.users() == 0);
-        CHECK_THROWS_WITH_AS(crate.probe(), "crate is not ready",  crate_error);
-        CHECK_THROWS_WITH_AS(crate.ready(), "crate is not ready",  crate_error);
-        CHECK_THROWS_WITH_AS(crate.probe(), "crate is not ready",  crate_error);
-        CHECK_THROWS_WITH_AS(crate[0], "module number out of range: 0",  crate_error);
-        CHECK_THROWS_WITH_AS(crate.find(0), "module slot not found",  crate_error);
+        CHECK_THROWS_WITH_AS(crate.probe(), "crate is not ready", crate_error);
+        CHECK_THROWS_WITH_AS(crate.ready(), "crate is not ready", crate_error);
+        CHECK_THROWS_WITH_AS(crate.probe(), "crate is not ready", crate_error);
+        CHECK_THROWS_WITH_AS(crate[0], "module number out of range: 0", crate_error);
+        CHECK_THROWS_WITH_AS(crate.find(0), "module slot not found", crate_error);
     }
     TEST_CASE("initialize") {
         using namespace xia::pixie;
@@ -77,14 +75,15 @@ TEST_SUITE("Crate: modules") {
         CHECK_NOTHROW(crate.probe());
         SUBCASE("Finding and offline") {
             CHECK_NOTHROW(crate.find(2));
-            CHECK_THROWS_WITH_AS(crate[-1], "module number out of range: -1",  crate_error);
-            CHECK_THROWS_WITH_AS(crate[20], "module number out of range: 20",  crate_error);
-            CHECK_THROWS_WITH_AS(crate.find(7), "module slot not found",  crate_error);
+            CHECK_THROWS_WITH_AS(crate[-1], "module number out of range: -1", crate_error);
+            CHECK_THROWS_WITH_AS(crate[20], "module number out of range: 20", crate_error);
+            CHECK_THROWS_WITH_AS(crate.find(7), "module slot not found", crate_error);
             CHECK(crate.num_modules == test_modules);
             CHECK(crate.modules.size() == test_modules);
             CHECK(crate.offline.size() == hw::max_slots - test_modules);
             CHECK(crate.backplane.sync_waiters.size() == crate.num_modules);
-            CHECK_THROWS_WITH_AS(crate.set_offline(7), "module number out of range: 7",  crate_error);
+            CHECK_THROWS_WITH_AS(crate.set_offline(7), "module number out of range: 7",
+                                 crate_error);
             CHECK_NOTHROW(crate.set_offline(1));
             CHECK(crate.num_modules == test_modules - 1);
             CHECK(crate.modules.size() == test_modules - 1);
@@ -97,7 +96,8 @@ TEST_SUITE("Crate: modules") {
             CHECK(crate[0].fifo_run_wait_usecs == module::module::default_fifo_run_wait_usec);
             CHECK(crate[0].fifo_idle_wait_usecs == module::module::default_fifo_idle_wait_usec);
             CHECK(crate[0].fifo_hold_usecs == module::module::default_fifo_hold_usec);
-            CHECK(crate[0].fifo_dma_trigger_level == module::module::default_fifo_dma_trigger_level);
+            CHECK(crate[0].fifo_dma_trigger_level ==
+                  module::module::default_fifo_dma_trigger_level);
             CHECK(crate[0].fifo_bandwidth == 0);
         }
         SUBCASE("FIFO bandwidth") {
@@ -111,12 +111,10 @@ TEST_SUITE("Crate: modules") {
             CHECK(crate[0].fifo_run_wait_usecs == module::module::max_fifo_run_wait_usec);
             CHECK_THROWS_WITH_AS(
                 crate[0].set_fifo_run_wait(module::module::min_fifo_run_wait_usec - 1),
-                "module: num=0,slot=2: fifo: run wait value out of range",
-                crate_error);
+                "module: num=0,slot=2: fifo: run wait value out of range", crate_error);
             CHECK_THROWS_WITH_AS(
                 crate[0].set_fifo_run_wait(module::module::max_fifo_run_wait_usec + 1),
-                "module: num=0,slot=2: fifo: run wait value out of range",
-                crate_error);
+                "module: num=0,slot=2: fifo: run wait value out of range", crate_error);
         }
         SUBCASE("FIFO idle wait") {
             CHECK_NOTHROW(crate[0].set_fifo_idle_wait(module::module::min_fifo_idle_wait_usec));
@@ -125,50 +123,45 @@ TEST_SUITE("Crate: modules") {
             CHECK(crate[0].fifo_idle_wait_usecs == module::module::max_fifo_idle_wait_usec);
             CHECK_THROWS_WITH_AS(
                 crate[0].set_fifo_idle_wait(module::module::min_fifo_idle_wait_usec - 1),
-                "module: num=0,slot=2: fifo: idle wait value out of range",
-                crate_error);
+                "module: num=0,slot=2: fifo: idle wait value out of range", crate_error);
             CHECK_THROWS_WITH_AS(
                 crate[0].set_fifo_idle_wait(module::module::max_fifo_idle_wait_usec + 1),
-                "module: num=0,slot=2: fifo: idle wait value out of range",
-                crate_error);
+                "module: num=0,slot=2: fifo: idle wait value out of range", crate_error);
         }
         SUBCASE("FIFO hold") {
             CHECK_NOTHROW(crate[0].set_fifo_hold(module::module::min_fifo_hold_usec));
             CHECK(crate[0].fifo_hold_usecs == module::module::min_fifo_hold_usec);
             CHECK_NOTHROW(crate[0].set_fifo_hold(module::module::max_fifo_hold_usec));
             CHECK(crate[0].fifo_hold_usecs == module::module::max_fifo_hold_usec);
-            CHECK_THROWS_WITH_AS(
-                crate[0].set_fifo_hold(module::module::min_fifo_hold_usec - 1),
-                "module: num=0,slot=2: fifo: hold value out of range",
-                crate_error);
-            CHECK_THROWS_WITH_AS(
-                crate[0].set_fifo_hold(module::module::max_fifo_hold_usec + 1),
-                "module: num=0,slot=2: fifo: hold value out of range",
-                crate_error);
+            CHECK_THROWS_WITH_AS(crate[0].set_fifo_hold(module::module::min_fifo_hold_usec - 1),
+                                 "module: num=0,slot=2: fifo: hold value out of range",
+                                 crate_error);
+            CHECK_THROWS_WITH_AS(crate[0].set_fifo_hold(module::module::max_fifo_hold_usec + 1),
+                                 "module: num=0,slot=2: fifo: hold value out of range",
+                                 crate_error);
         }
         SUBCASE("FIFO DMA trigger level") {
-            CHECK_NOTHROW(crate[0].set_fifo_dma_trigger_level(module::module::min_fifo_dma_trigger_level));
+            CHECK_NOTHROW(
+                crate[0].set_fifo_dma_trigger_level(module::module::min_fifo_dma_trigger_level));
             CHECK(crate[0].fifo_dma_trigger_level == module::module::min_fifo_dma_trigger_level);
-            CHECK_NOTHROW(crate[0].set_fifo_dma_trigger_level(module::module::max_fifo_dma_trigger_level));
+            CHECK_NOTHROW(
+                crate[0].set_fifo_dma_trigger_level(module::module::max_fifo_dma_trigger_level));
             CHECK(crate[0].fifo_dma_trigger_level == module::module::max_fifo_dma_trigger_level);
             CHECK_THROWS_WITH_AS(
                 crate[0].set_fifo_dma_trigger_level(module::module::min_fifo_dma_trigger_level - 1),
-                "module: num=0,slot=2: fifo: dma trigger level value out of range",
-                crate_error);
+                "module: num=0,slot=2: fifo: dma trigger level value out of range", crate_error);
             CHECK_THROWS_WITH_AS(
                 crate[0].set_fifo_dma_trigger_level(module::module::max_fifo_dma_trigger_level + 1),
-                "module: num=0,slot=2: fifo: dma trigger level value out of range",
-                crate_error);
+                "module: num=0,slot=2: fifo: dma trigger level value out of range", crate_error);
         }
         SUBCASE("FIFO bandwidth") {
             CHECK_NOTHROW(crate[0].set_fifo_bandwidth(0));
             CHECK(crate[0].fifo_bandwidth == 0);
             CHECK_NOTHROW(crate[0].set_fifo_bandwidth(100));
             CHECK(crate[0].fifo_bandwidth == 100);
-            CHECK_THROWS_WITH_AS(
-                crate[0].set_fifo_bandwidth(101),
-                "module: num=0,slot=2: fifo: bandwidth value out of range",
-                crate_error);
+            CHECK_THROWS_WITH_AS(crate[0].set_fifo_bandwidth(101),
+                                 "module: num=0,slot=2: fifo: bandwidth value out of range",
+                                 crate_error);
         }
     }
     TEST_CASE("assign slots") {
@@ -216,63 +209,76 @@ TEST_SUITE("Crate: modules") {
         sim::crate crate;
         CHECK_NOTHROW(crate.initialize());
         CHECK_NOTHROW(crate.probe());
-        /* wired-or trigger pullup */
-        CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CPLDPULLUP));
-        CHECK_THROWS_WITH_AS(
-            crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CPLDPULLUP),
-            "module: num=2,slot=10: wired-or trigger pullups leader role already taken: 0", crate_error);
-        CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 0));
-        CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CPLDPULLUP));
-        CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 0));
-        /* director mode */
-        CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_DIRMOD));
-        CHECK_THROWS_WITH_AS(
-            crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_DIRMOD),
-            "module: num=2,slot=10: director leader role already taken: 0", crate_error);
-        CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 0));
-        CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_DIRMOD));
-        CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 0));
-        /* run leader */
-        CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CHASSISMASTER));
-        CHECK_THROWS_WITH_AS(
-            crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CHASSISMASTER),
-            "module: num=2,slot=10: run leader role already taken: 0", crate_error);
-        CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 0));
-        CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CHASSISMASTER));
-        CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 0));
-        /* no synch wait */
-        CHECK(crate.backplane.sync_waits == 0);
-        CHECK(crate.backplane.sync_waiters[0] == false);
-        CHECK(crate.backplane.sync_waiters[1] == false);
-        CHECK(crate.backplane.sync_waiters[2] == false);
-        CHECK_NOTHROW(crate[0].write(module_param::synch_wait, 0));
-        CHECK(crate.backplane.sync_waits == 0);
-        CHECK(crate.backplane.sync_waiters[0] == false);
-        CHECK(crate.backplane.sync_waiters[1] == false);
-        CHECK(crate.backplane.sync_waiters[2] == false);
-        CHECK_NOTHROW(crate[0].start_histograms(hw::run::run_mode::new_run));
-        CHECK_NOTHROW(crate[0].run_end());
-        /* incomplete synch wait */
-        CHECK_NOTHROW(crate[0].write(module_param::synch_wait, 1));
-        CHECK(crate.backplane.sync_waits == 1);
-        CHECK(crate.backplane.sync_waiters[0] == true);
-        CHECK(crate.backplane.sync_waiters[1] == false);
-        CHECK(crate.backplane.sync_waiters[2] == false);
-        CHECK_THROWS_WITH_AS(
-            crate[0].start_histograms(hw::run::run_mode::new_run),
-            "module: num=0,slot=2: sync wait mode enabled and not all modules in sync wait state",
-            crate_error);
-        CHECK_NOTHROW(crate[0].run_end());
-        /* complete synch wait */
-        CHECK_NOTHROW(crate[0].write(module_param::synch_wait, 1));
-        CHECK_NOTHROW(crate[1].write(module_param::synch_wait, 1));
-        CHECK_NOTHROW(crate[2].write(module_param::synch_wait, 1));
-        CHECK(crate.backplane.sync_waits == test_modules);
-        CHECK(crate.backplane.sync_waiters[0] == true);
-        CHECK(crate.backplane.sync_waiters[1] == true);
-        CHECK(crate.backplane.sync_waiters[2] == true);
-        /* run end check */
-        CHECK_NOTHROW(crate[0].start_histograms(hw::run::run_mode::new_run));
-        CHECK_NOTHROW(crate[0].run_end());
+        SUBCASE("wired-or trigger pullup") {
+            CHECK_NOTHROW(
+                crate[0].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CPLDPULLUP));
+            CHECK_THROWS_WITH_AS(
+                crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CPLDPULLUP),
+                "module: num=2,slot=10: wired-or trigger pullups leader role already taken: 0",
+                crate_error);
+            CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 0));
+            CHECK_NOTHROW(
+                crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CPLDPULLUP));
+            CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 0));
+        }
+        SUBCASE("director mode") {
+            CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_DIRMOD));
+            CHECK_THROWS_WITH_AS(
+                crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_DIRMOD),
+                "module: num=2,slot=10: director leader role already taken: 0", crate_error);
+            CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 0));
+            CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_DIRMOD));
+            CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 0));
+        }
+        SUBCASE("run leader") {
+            CHECK_NOTHROW(
+                crate[0].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CHASSISMASTER));
+            CHECK_THROWS_WITH_AS(
+                crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CHASSISMASTER),
+                "module: num=2,slot=10: run leader role already taken: 0", crate_error);
+            CHECK_NOTHROW(crate[0].write(module_param::module_csrb, 0));
+            CHECK_NOTHROW(
+                crate[2].write(module_param::module_csrb, 1 << hw::bit::MODCSRB_CHASSISMASTER));
+            CHECK_NOTHROW(crate[2].write(module_param::module_csrb, 0));
+        }
+        SUBCASE("no synch wait") {
+            CHECK(crate.backplane.sync_waits == 0);
+            CHECK(crate.backplane.sync_waiters[0] == false);
+            CHECK(crate.backplane.sync_waiters[1] == false);
+            CHECK(crate.backplane.sync_waiters[2] == false);
+            CHECK_NOTHROW(crate[0].write(module_param::synch_wait, 0));
+            CHECK(crate.backplane.sync_waits == 0);
+            CHECK(crate.backplane.sync_waiters[0] == false);
+            CHECK(crate.backplane.sync_waiters[1] == false);
+            CHECK(crate.backplane.sync_waiters[2] == false);
+            CHECK_NOTHROW(crate[0].start_histograms(hw::run::run_mode::new_run));
+            CHECK_NOTHROW(crate[0].run_end());
+        }
+        SUBCASE("incomplete synch wait") {
+            CHECK_NOTHROW(crate[0].write(module_param::synch_wait, 1));
+            CHECK(crate.backplane.sync_waits == 1);
+            CHECK(crate.backplane.sync_waiters[0] == true);
+            CHECK(crate.backplane.sync_waiters[1] == false);
+            CHECK(crate.backplane.sync_waiters[2] == false);
+            CHECK_THROWS_WITH_AS(
+                crate[0].start_histograms(hw::run::run_mode::new_run),
+                "module: num=0,slot=2: sync wait mode enabled and not all modules in sync wait state",
+                crate_error);
+            CHECK_NOTHROW(crate[0].run_end());
+        };
+        SUBCASE("complete synch wait") {
+            CHECK_NOTHROW(crate[0].write(module_param::synch_wait, 1));
+            CHECK_NOTHROW(crate[1].write(module_param::synch_wait, 1));
+            CHECK_NOTHROW(crate[2].write(module_param::synch_wait, 1));
+            CHECK(crate.backplane.sync_waits == test_modules);
+            CHECK(crate.backplane.sync_waiters[0] == true);
+            CHECK(crate.backplane.sync_waiters[1] == true);
+            CHECK(crate.backplane.sync_waiters[2] == true);
+            CHECK_NOTHROW(crate[0].start_histograms(hw::run::run_mode::new_run));
+            CHECK_NOTHROW(crate[0].run_end());
+        }
+    }
+    TEST_CASE("TEARDOWN") {
+        xia::logging::stop("log");
     }
 }
