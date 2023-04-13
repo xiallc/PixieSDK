e85693223002504ce97c269a155013af91b495b9
S. V. Paulauskas <stan@xia.com>
2023-04-13 15:23:57 -0400

    P16-719: Updating list_mode_file_stats.cpp to handle multiple files at once.
    Signed-off-by: S. V. Paulauskas <stan@xia.com>

--- examples/list_mode_file_stats/list_mode_file_stats.cpp
+++ examples/list_mode_file_stats/list_mode_file_stats.cpp
@@ -65,7 +65,9 @@ struct config {
     uint32_t frequency;
     uint32_t revision;
     uint32_t slot;
-    config() : frequency(0), revision(0), slot(0) {}
+    uint32_t bits{};
+    std::vector<std::string> files;
+    config() : frequency(0), revision(0), slot(0), bits(0) {}
 };
 
 using configs = std::vector<config>;
@@ -112,6 +114,10 @@ void verify_json_slot(const nlohmann::json& node) {
     if (!node.contains("frequency")) {
         throw std::invalid_argument("Missing frequency object in configuration element.");
     }
+
+    if (!node.contains("files")) {
+        throw std::invalid_argument("Missing files list in configuration element.");
+    }
 }
 
 void read_config(const std::string& file, configs& cfgs) {
@@ -126,11 +132,12 @@ void read_config(const std::string& file, configs& cfgs) {
     for (const auto& element : jf) {
         verify_json_slot(element);
 
-        config mod_cfg;
-        mod_cfg.slot = element["slot"];
-        mod_cfg.revision = element["revision"];
-        mod_cfg.frequency = element["frequency"];
-        cfgs.push_back(mod_cfg);
+        config cfg;
+        cfg.slot = element["slot"];
+        cfg.revision = element["revision"];
+        cfg.frequency = element["frequency"];
+        cfg.files = element["files"].get<std::vector<std::string>>();
+        cfgs.push_back(cfg);
     }
 }
 
@@ -151,9 +158,9 @@ int main(int argc, char** argv) {
         arguments, "cfg", "The configuration file telling us what to expect in the list-mode data.",
         {'c', "config"}, args::Options::Required);
     args::HelpFlag help_flag(arguments, "help", "Displays this message", {'h', "help"});
-    args::ValueFlag<std::string> input_flag(arguments, "input_file",
-                                            "The input file we'll attempt to decode.",
-                                            {'i', "input-file"}, args::Options::Required);
+    args::ValueFlag<std::string> output_flag(
+        arguments, "output_file",
+        "The name of the JSONL file that will contain all the extracted records.", {'o', "output"});
 
     try {
         parser.ParseCLI(argc, argv);
@@ -179,129 +186,161 @@ int main(int argc, char** argv) {
               << calculate_duration_in_seconds(start, std::chrono::system_clock::now()) << " s."
               << std::endl;
 
-    start = std::chrono::system_clock::now();
-    std::cout << LOG("INFO") << "Starting to parse " << input_flag.Get() << std::endl;
+    std::ofstream output;
+    if (output_flag) {
+        output.open(output_flag.Get());
+    }
 
-    std::ifstream input(input_flag.Get(), std::ios::in | std::ios::binary | std::ios::ate);
-    xia::pixie::data::list_mode::buffer remainder;
-    auto record_total = 0;
-    channel_stats stats;
-    try {
-        if (input.fail()) {
-            throw std::ios_base::failure("open: " + input_flag.Get() + ": " + std::strerror(errno));
-        }
+    for (const auto& cfg : cfgs) {
+        for (const auto& file : cfg.files) {
+            start = std::chrono::system_clock::now();
+            std::cout << LOG("INFO") << "Starting to parse " << file << std::endl;
 
-        static const auto bytes_per_word = 4;
-        /*
-         * This is the approximate maximum size of a Rev H record (header + trace).
-         */
-        static const auto chunk_size_words = 16398;
-        auto size_bytes = input.tellg();
-        auto size_words = size_bytes / bytes_per_word;
-        auto num_chunks = size_words / chunk_size_words;
-        auto data_vec_size = chunk_size_words;
-        auto max_retries = 10;
-        auto retries = max_retries;
-
-        if (size_words < chunk_size_words) {
-            data_vec_size = int(size_words);
-        }
-
-        std::cout << LOG("INFO") << "File Size In Bytes: " << size_bytes
-                  << " | File Size In Words: " << size_words << std::endl;
-
-        input.seekg(0);
-        std::cout << LOG("INFO") << "Starting to decode data." << std::endl;
-        for (int chunk_num = 0; chunk_num <= num_chunks; chunk_num++) {
-            if (retries == 0) {
-                std::cout << LOG("ERROR") << "Failed to resync data stream." << std::endl;
-                break;
-            }
-
-            xia::pixie::data::list_mode::buffer data(data_vec_size, 0);
-            input.read(reinterpret_cast<char*>(&data[0]), chunk_size_words * bytes_per_word);
-            if (!remainder.empty()) {
-                data.insert(data.begin(), remainder.begin(), remainder.end());
-                remainder.clear();
-                remainder.shrink_to_fit();
-            }
+            std::ifstream input(file, std::ios::in | std::ios::binary | std::ios::ate);
+            xia::pixie::data::list_mode::buffer remainder;
+            auto record_total = 0;
+            channel_stats stats;
             try {
-                xia::pixie::data::list_mode::records records;
-                xia::pixie::data::list_mode::decode_data_block(
-                    data, cfgs[0].revision, cfgs[0].frequency, records, remainder);
-                record_total += int(records.size());
-                for (const auto& record : records) {
-                    auto stat_rec = stats.find(record.channel_number);
-                    if (stat_rec == stats.end()) {
-                        stats[record.channel_number] = {record.channel_number,
-                                                        1,
-                                                        0,
-                                                        record.energy,
-                                                        record.energy,
-                                                        record.energy,
-                                                        double(record.event_length),
-                                                        double(record.header_length),
-                                                        record.time.count(),
-                                                        record.time.count(),
-                                                        double(record.trace_length)};
-                    } else {
-                        auto ch = &stat_rec->second;
-                        ch->count++;
-                        ch->energy_ave += record.energy;
-                        ch->trace_length_ave += record.trace_length;
-                        ch->header_length_ave += record.header_length;
-                        ch->event_length_ave += record.event_length;
+                if (input.fail()) {
+                    throw std::ios_base::failure("open: " + file + ": " + std::strerror(errno));
+                }
 
-                        if (record.energy > ch->energy_max) {
-                            ch->energy_max = record.energy;
+                static const auto bytes_per_word = 4;
+                /*
+                 * This is the approximate maximum size of a Rev H record (header + trace).
+                 */
+                static const auto chunk_size_words = 16398;
+                auto size_bytes = input.tellg();
+                auto size_words = size_bytes / bytes_per_word;
+                auto num_chunks = size_words / chunk_size_words;
+                auto data_vec_size = chunk_size_words;
+                auto max_retries = 10;
+                auto retries = max_retries;
+
+                if (size_words < chunk_size_words) {
+                    data_vec_size = int(size_words);
+                }
+
+                std::cout << LOG("INFO") << "File Size In Bytes: " << size_bytes
+                          << " | File Size In Words: " << size_words << std::endl;
+
+                input.seekg(0);
+                std::cout << LOG("INFO") << "Starting to decode data." << std::endl;
+                for (int chunk_num = 0; chunk_num <= num_chunks; chunk_num++) {
+                    if (retries == 0) {
+                        std::cout << LOG("ERROR") << "Failed to resync data stream." << std::endl;
+                        break;
+                    }
+
+                    xia::pixie::data::list_mode::buffer data(data_vec_size, 0);
+                    input.read(reinterpret_cast<char*>(&data[0]),
+                               chunk_size_words * bytes_per_word);
+                    if (!remainder.empty()) {
+                        data.insert(data.begin(), remainder.begin(), remainder.end());
+                        remainder.clear();
+                        remainder.shrink_to_fit();
+                    }
+                    try {
+                        xia::pixie::data::list_mode::records records;
+                        xia::pixie::data::list_mode::decode_data_block(
+                            data, cfgs[0].revision, cfgs[0].frequency, records, remainder);
+                        record_total += int(records.size());
+                        for (const auto& record : records) {
+                            if (output.is_open()) {
+                                std::string out;
+                                xia::pixie::data::list_mode::record_to_json(record, out);
+                                output << out << std::endl;
+                            }
+                            auto stat_rec = stats.find(record.channel_number);
+                            if (stat_rec == stats.end()) {
+                                stats[record.channel_number] = {record.channel_number,
+                                                                1,
+                                                                0,
+                                                                record.energy,
+                                                                record.energy,
+                                                                record.energy,
+                                                                double(record.event_length),
+                                                                double(record.header_length),
+                                                                record.time.count(),
+                                                                record.time.count(),
+                                                                double(record.trace_length)};
+                            } else {
+                                auto ch = &stat_rec->second;
+                                ch->count++;
+                                ch->energy_ave += record.energy;
+                                ch->trace_length_ave += double(record.trace_length);
+                                ch->header_length_ave += record.header_length;
+                                ch->event_length_ave += record.event_length;
+
+                                if (record.energy > ch->energy_max) {
+                                    ch->energy_max = record.energy;
+                                }
+                                if (record.energy < ch->energy_min) {
+                                    ch->energy_min = record.energy;
+                                }
+                                if (record.time.count() > ch->time_max) {
+                                    ch->time_max = record.time.count();
+                                }
+                                if (record.time.count() < ch->time_min) {
+                                    ch->time_min = record.time.count();
+                                }
+                            }
                         }
-                        if (record.energy < ch->energy_min) {
-                            ch->energy_min = record.energy;
+                        if (retries != max_retries) {
+                            std::cout << LOG("INFO") << "Successfully synchronized data stream."
+                                      << std::endl;
+                            retries = max_retries;
                         }
-                        if (record.time.count() > ch->time_max) {
-                            ch->time_max = record.time.count();
+                    } catch (xia::pixie::data::list_mode::error& error) {
+                        std::cout << LOG("WARN") << "Decoding failed on chunk " << chunk_num - 1
+                                  << " starting at byte "
+                                  << (chunk_num - 1) * chunk_size_words * bytes_per_word
+                                  << " with the following error."
+                                  << std::endl;
+                        std::cout << LOG("ERROR") << error.what() << std::endl;
+                        if (chunk_num == num_chunks) {
+                            std::cout << LOG("INFO") << "Possible data loss in last chunk." << std::endl;
+                        } else {
+                            std::cout << LOG("INFO") << "Attempting to resync data stream." << std::endl;
                         }
-                        if (record.time.count() < ch->time_min) {
-                            ch->time_min = record.time.count();
-                        }
+                        retries--;
                     }
                 }
-                retries = max_retries;
-            } catch (xia::pixie::data::list_mode::error& error) {
-                std::cout << LOG("WARN") << "Decoding failed on chunk " << chunk_num - 1
-                          << " starting at byte " << (chunk_num-1) * chunk_size_words * bytes_per_word
-                          << " with the following error. Attempting to resync data stream." << std::endl;
-                std::cout << LOG("ERROR") << error.what() << std::endl;
-                retries--;
+                std::cout << LOG("INFO") << "Finished decoding data in "
+                          << calculate_duration_in_seconds(start, std::chrono::system_clock::now())
+                          << " s." << std::endl;
+                input.close();
+            } catch (std::bad_alloc& bad_alloc) {
+                std::cout << LOG("ERROR") << bad_alloc.what() << std::endl;
+            } catch (std::ios_base::failure& failure) {
+                std::cout << LOG("ERROR") << failure.what() << std::endl;
+            } catch (xia::pixie::error::error& sdkerr) {
+                std::cout << LOG("ERROR") << sdkerr.what() << std::endl;
             }
+
+            std::cout << LOG("INFO") << "Total records processed : " << record_total << std::endl;
+
+            for (auto& stat : stats) {
+                auto ch = &stat.second;
+                ch->energy_ave /= double(ch->count);
+                ch->cps = double(ch->count) / (ch->time_max - ch->time_min);
+                ch->header_length_ave /= double(ch->count);
+                ch->trace_length_ave /= double(ch->count);
+                ch->event_length_ave /= double(ch->count);
+                nlohmann::json j = *ch;
+                std::cout << LOG("INFO") << j.dump() << std::endl;
+            }
+
+            if (!remainder.empty()) {
+                std::cout << LOG("WARN") << "Leftover Words: " << remainder.size() << std::endl;
+            }
+
+            std::cout << LOG("INFO") << "Finished parsing " << file << std::endl;
         }
-        std::cout << LOG("INFO") << "Finished decoding data in "
-                  << calculate_duration_in_seconds(start, std::chrono::system_clock::now()) << " s."
-                  << std::endl;
-        input.close();
-    } catch (std::bad_alloc& bad_alloc) {
-        std::cout << LOG("ERROR") << bad_alloc.what() << std::endl;
-    } catch (std::ios_base::failure& failure) {
-        std::cout << LOG("ERROR") << failure.what() << std::endl;
-    } catch (xia::pixie::error::error& sdkerr) {
-        std::cout << LOG("ERROR") << sdkerr.what() << std::endl;
     }
 
-    std::cout << LOG("INFO") << "Total records processed : " << record_total << std::endl;
-
-    for (auto& stat : stats) {
-        auto ch = &stat.second;
-        ch->energy_ave /= ch->count;
-        ch->cps = ch->count / (ch->time_max - ch->time_min);
-        ch->header_length_ave /= ch->count;
-        ch->trace_length_ave /= ch->count;
-        ch->event_length_ave /= ch->count;
-        nlohmann::json j = *ch;
-        std::cout << LOG("INFO") << j.dump() << std::endl;
-    }
-
-    if (!remainder.empty()) {
-        std::cout << LOG("WARN") << "Leftover Words: " << remainder.size() << std::endl;
+    if(output.is_open()) {
+        output.close();
     }
 
     std::cout << LOG("INFO") << "Finished execution in "
